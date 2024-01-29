a9aa1a77978ee8f162d41a79a5e4fa1a718b4bf6
Michele Olivieri <micheleolivieri02@gmail.com>
2024-01-29 18:09:10 +0100

    first implementation of home.php

--- src/views/home.php
+++ src/views/home.php
@@ -1,12 +1,14 @@
 <?php
-include_once('../models/database.php');
+include_once('../db/database.php');
 
 // Assume the user is logged in and you have the user ID
-$loggedInUserID = 2; //Replace with the actual logged-in user ID
+$loggedInUserID = 1; //Replace with the actual logged-in user ID
 
 $database = new Database();
 
 $userData = $database->getUserByID($loggedInUserID);
+
+$posts = $database->getPostFromFollowing($loggedInUserID);
 
 ?>
 
@@ -43,29 +45,33 @@ $userData = $database->getUserByID($loggedInUserID);
          <form>
              <input type="text" id="Search" placeholder="Search for your artists" title="search bar">
          </form>
-         <div class="post-container" id="Audemars_Piguet">
-            <div class="post">
-                <header>
-                    <img src="../img/ProfileImg.png" alt="User Avatar">
-                    <a href="#" aria-label="View user profile">Audemars Piguet</a>
-                </header>
-                <img src="../img/audemars_piguet.jpg" alt="Post Image">
-                <p>The temporary exhibition, "Simply Complicated," highlights the skill and watchmaking expertise of the Code 11.59 by Audemars Piguet Universelle.</p>
-                <section>
-                    <h1>interaction</h1>
-                    <img class="icon likeButton" src="../icon/like-empty.svg" alt="like button" onclick="changeLike()">
-                    <img class="icon commentButton" src="../icon/comment-empty.svg" alt="comment button" onclick="changeComment(/*passare l'id del post*/)">
-                    <img class="icon shareButton" src="../icon/share.svg" alt="comment button" onclick="share()">                
-                </section>
-            </div>
-            <aside class="comments">
-                <h2>Commenti</h2>
-                <p class="description">The temporary exhibition, "Simply Complicated," highlights the skill and watchmaking expertise of the Code 11.59 by Audemars Piguet Universelle.</p>
-                <p>primo commento</p>
-                <p>secondo commento</p>
-                <input type="text" placeholder="aggiungi un commento" title="add comment">
-            </aside>
-        </div>
+         <?php if (!empty($posts)) : ?>
+            <?php foreach ($posts as $post) : ?>
+                <div class="post-container" id="<?php $post['PostID']; ?>">
+                    <div class="post">
+                        <header>
+                            <img src="../img/ProfileImg.png" alt="User Avatar">
+                            <a href="#" label="username"><?php echo $post['UserID']; ?></a>
+                        </header>
+                        <img src="<?php echo $post['MediaURL']; ?>" alt="Post Image">
+                        <p><?php echo $post['Caption']; ?></p>
+                        <section>
+                            <h1>interaction</h1>
+                            <img class="icon likeButton" src="../icon/like-empty.svg" alt="like button" onclick="changeLike()">
+                            <img class="icon commentButton" src="../icon/comment-empty.svg" alt="comment button" onclick="changeComment(/*passare l'id del post*/)">
+                            <img class="icon shareButton" src="../icon/share.svg" alt="comment button" onclick="share()">                
+                        </section>
+                    </div>
+                    <aside class="comments">
+                        <h2>Commenti</h2>
+                        <p class="description"><?php echo $post['Caption']; ?></p>
+                        <p>primo commento</p>
+                        <p>secondo commento</p>
+                        <input type="text" placeholder="aggiungi un commento" title="add comment">
+                    </aside>
+                </div>
+            <?php endforeach; ?>
+        <?php else : echo "Nessun post disponibile dai seguiti."; endif; ?>
         <div class="space">
             <p>space</p>
         </div>
@@ -76,66 +82,4 @@ $userData = $database->getUserByID($loggedInUserID);
     <script src="../js/post.js"></script>
     <script src="../js/home.js"></script>
  </body>
-</html>
-
-
-<!DOCTYPE html>
-<html>
-    <head>
-            <meta charset="utf-8">
-            <title>jQuery Infinite Scroll</title>
-    </head>
-    <body>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script type="text/javascript">
-            var start = 0;
-            var working = false;
-            $(document).ready(function() {
-                $.ajax({
-                    type: "GET",
-                    url: "data.php?start="+start,
-                    processData: false,
-                    contentType: "application/json",
-                    data: '',
-                    success: function(r) {
-                        r = JSON.parse(r)
-                        for (var i = 0; i < r.length; i++) {
-                                $('body').append("<div><h1>"+r[i].videoName+"</h1><h2>Video Views: "+r[i].videoViews+"</h2></div>")
-                        }
-                        start += 5;
-                    },
-                    error: function(r) {
-                        console.log("Something went wrong!");
-                    }
-                })
-            })
-            $(window).scroll(function() {
-                if ($(this).scrollTop() + 1 >= $('body').height() - $(window).height()) {
-                    if (working == false) {
-                        working = true;
-                        $.ajax({
-                            type: "GET",
-                            url: "data.php?start="+start,
-                            processData: false,
-                            contentType: "application/json",
-                            data: '',
-                            success: function(r) {
-                                r = JSON.parse(r)
-                                for (var i = 0; i < r.length; i++) {
-                                        $('body').append("<div><h1>"+r[i].videoName+"</h1><h2>Video Views: "+r[i].videoViews+"</h2></div>")
-                                }
-                                start += 5;
-                                setTimeout(function() {
-                                        working = false;
-                                }, 4000)
-                            },
-                            error: function(r) {
-                                console.log("Something went wrong!");
-                            }
-                        });
-                    }
-                }
-            })
-        </script>
-    </body>
 </html>
