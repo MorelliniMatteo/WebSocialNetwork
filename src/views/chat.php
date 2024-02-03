7ea650e6c5d083713fb092cf5fa56c8eefe66eb2
Ji Junkai <junkai.ji@studio.unibo.it>
2024-02-03 21:28:32 +0100

    added nav to chat

--- src/views/chat.php
+++ src/views/chat.php
@@ -20,6 +20,9 @@ $path = "../img/chat/";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat</title>
+    
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/navbar.css">
     <link rel="stylesheet" href="../css/chat.css">
 </head>
 <body>
@@ -72,8 +75,10 @@ $path = "../img/chat/";
             </div>
 
     </main>
-
 </main>
+
+<?php include_once('Nav.php'); ?>
+
 
 </body>
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
