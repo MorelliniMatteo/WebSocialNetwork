20c3b4d43cfa0e6360e47c9b8d7078566cf2c93f
Ji Junkai <junkai.ji@studio.unibo.it>
2024-02-03 15:13:52 +0100

    add switch page effect

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,2 +1,21 @@
-var chat = document.getElementById("chat");
-chat.scrollTop = chat.scrollHeight - chat.clientHeight;
+// chat.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const messageButtons = document.querySelectorAll('.message');
+    messageButtons.forEach(button => {
+        button.addEventListener('click', showDialogContainer);
+    });
+
+    document.getElementById('backButton').addEventListener('click', showMessageContainer);
+});
+
+function showDialogContainer() {
+    document.querySelector('.message-container').style.display = 'none';
+    document.querySelector('.dialog-page').style.display = 'flex';
+}
+
+function showMessageContainer() {
+    document.querySelector('.message-container').style.display = 'flex';
+    document.querySelector('.dialog-page').style.display = 'none';
+}
