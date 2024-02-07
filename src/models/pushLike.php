25440961bbb8db248fcc964f56e3b8c227ab9ac9
Michele Olivieri <micheleolivieri02@gmail.com>
2024-02-07 18:25:16 +0100

    fix bug on like from post.php

--- src/models/pushLike.php
+++ src/models/pushLike.php
@@ -1,4 +1,18 @@
 <?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Verifica se l'utente è autenticato
+if (!isset($_SESSION['user_id'])) {
+    // Utente non autenticato, potresti reindirizzarlo alla pagina di login
+    header('Location: login.php');
+    exit();
+}
+
+// Ottieni l'ID dell'utente dalla sessione
+$loggedInUserID = $_SESSION['user_id'];
+
 include_once('../db/database.php');
 
 $database = new Database();
@@ -6,7 +20,7 @@ $database = new Database();
 // Verifica se sono stati ricevuti i dati corretti tramite POST
 if (isset($_POST['postID']) && isset($_POST['userID']) && isset($_POST['add'])) {
     $postID = $_POST['postID'];
-    $userID = $_POST['userID'];
+    $userID = $loggedInUserID;
     $add = $_POST['add'];
     echo $add;
     
